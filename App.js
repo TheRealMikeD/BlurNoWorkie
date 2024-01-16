b211c688d778301ced63760618bbe9e27852fb4d
Mike Davis <mdavis@duckandbeaver.com>
2024-01-16 15:22:43 -0700

    First commit.

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, SafeAreaView, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const FirstScreen = ({navigation}) => {
+  function goToSecond() {
+    navigation.navigate('second');
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        This is the first screen.
+      </Text>
+      <Button style={styles.btn} onPress={goToSecond} title="Go to Second Screen"/>
+    </SafeAreaView>
+  );
+};
+
+const SecondScreen = ({navigation}) => {
+	useEffect(() => {
+		// Add a focus listener.
+		const unsubscribe = navigation.addListener('focus', () => {
+			console.log(`[SecondScreen->useEffect->focus] focus listener fired.`);
+		});
+
+		return unsubscribe;
+	}, [navigation]);
+
+	useEffect(() => {
+		// Add a blur listener.
+		const unsubscribe = navigation.addListener('blur', () => {
+			console.log(`[SecondScreen->useEffect->blur] blur listener fired.`);
+		});
+
+		return unsubscribe;
+	}, [navigation]);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        This is the second screen.
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+function MyNav() {
+	// Show the navigation for an unauthenticated user.
+	return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="first" component={FirstScreen}/>
+        <Stack.Screen name="second" component={SecondScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+	);
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MyNav/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  btn: {
+    maxWidth: 200,
+    color: '#FFF'
+  }
 });
